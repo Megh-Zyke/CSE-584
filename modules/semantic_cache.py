68b418cec238663c112029634983aec03e79073b
Megh-Zyke <meghanand1234@gmail.com>
2026-04-14 12:52:37 -0400

    added missing locks

--- modules/semantic_cache.py
+++ modules/semantic_cache.py
@@ -1,6 +1,6 @@
 import torch 
 from transformers import AutoModelForSequenceClassification, AutoTokenizer
-
+import numpy as np
 class PyFSSemanticCache:
 
     def __init__(self, model_name="cross-encoder/stsb-roberta-base"):
